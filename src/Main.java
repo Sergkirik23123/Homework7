0e89f10e27b9876d9decd1ad5936a81b5b5e3c71
sergkirik <serrh23123@gmail.com>
2025-02-12 21:35:17 +0200

    class Animal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
 
+        Animal dog = new Animal("Рекс", 1, "Шпиц", 1000);
+
+        dog.displayInfo();
+
     }
+}
+
+class Animal{
+    String name;
+    int age;
+    String breed;
+    int prise;
+
+    public Animal(){
+        name = "Unknown";
+        age = 0;
+        breed = "Unknown";
+        prise = 0;
+
+    }
+
+    public Animal(String name, int age, String breed, int prise) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.prise = prise;
+    }
+
+    public void displayInfo(){
+        System.out.println("Кличка - " + name + ": " + "Возраст - " + age + ": " + "Порода - " + breed + ": " + "Цена - " + prise);
+
+    }
+
 }
